d53a1bece5b82362de1cec6c4465c74487bab4b7
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-01-18 00:19:20 +0900

    비밀번호 암호화 까지 완료 21/01/18

--- loginWeb/index.js
+++ loginWeb/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 5000
 const bodyParser = require('body-parser');
 const {User}=require("./models/User");// 회원가입에 필요한 정보를 가져오기 위한것
+const mongoose = require('mongoose');
 
 const config = require("./config/key");
 
@@ -13,23 +14,18 @@ app.use(bodyParser.urlencoded({extended : true}));
 //application/json 이파일을 분석해서 가져오게 하는것
 app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
 }).then(()=>console.log('MongoDB Connected...')).catch(err=>console.log(err))
 
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.get('/', (req, res) => res.send('Hello World!'))
+ 
 app.post('/register',(req,res)=>{
   //회원가입시 핑요한 정보들을 Client에서 가져오면
   //그것을 데이터 베이스에 넣는다. 
-  const user = new User(res.body);
+  const user = new User(req.body);
+  
   user.save((err,userInfo)=>{
     if(err)return res.json({success : false, err})
     return res.status(200).json({
@@ -38,6 +34,4 @@ app.post('/register',(req,res)=>{
   })//이것은 몽고 DB에서오는 메소드 
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- loginWeb/models/User.js
+++ loginWeb/models/User.js
@@ -1,4 +1,6 @@
 const mongoose  = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10 // 열자리를 라운드처리하는것
 
 const userSchema = mongoose.Schema({
     name : {
@@ -30,5 +32,26 @@ const userSchema = mongoose.Schema({
         type : Number
     },// 유효 기간 설정
 })
+
+userSchema.pre('save',function(next){
+    var user = this;
+    //비밀 번호를 암호화 시키기
+//우리가 이메일이나 이름 바꿀때 마다 비번이 암호화된다면 낭비이니까
+
+if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)// 오류가 나면 이곳으로 
+
+
+        bcrypt.hash(user.password, salt, function(err, hash) {//암호화된 비번
+            // Store hash in your password DB.
+            if(err) return next(err)
+            user.password =hash //성공하면 암호화된 비번으로 바꿈
+            next()//되돌아가기
+        })
+    })
+ }
+})
+
 const User = mongoose.model('User',userSchema)
 module.exports = {User};// 이모델을 다른곳에서 사용 하기위해서 
