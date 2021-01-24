08882e78284a9d05939ba7cb61c00f239409f366
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-01-24 16:31:39 +0900

    리덕스 설정 (21/01/23)

--- loginWeb/client/src/index.js
+++ loginWeb/client/src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './reducers';
 
-
+const createStoreWithMiddelware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store = {(createStoreWithMiddelware(),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
