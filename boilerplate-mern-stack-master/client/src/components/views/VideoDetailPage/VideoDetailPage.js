ed3df8840f5549b99f53816d667b594573e65195
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-03-10 20:32:35 +0900

    [21.03.10] 사이드비디오 생성

--- boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {Row, Col, List, Avatar} from 'antd';
 import Axios from 'axios';
+import SideVideo from './Sections/SideVideo'
 function VideoDetailPage(props) {
     //비디오 아이디 가져오는 법
     const videoId = props.match.params.videoId
@@ -40,7 +41,7 @@ function VideoDetailPage(props) {
             </div>
             </Col>
             <Col lg ={6} xs={24}>
-                side Videos
+                <SideVideo />
             </Col>
         </Row>
     )
